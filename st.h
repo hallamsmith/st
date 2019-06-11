b866533ed6ab0bec071b46f47f1e720e6a0a3f65
Hallam Smith <smithhallam@gmail.com>
2019-06-11 13:48:36 +0100

    Changes to be committed: 	modified:   config.def.h 	modified:   st.h 	modified:   x.c

--- st.h
+++ st.h
@@ -76,13 +76,6 @@ typedef union {
 	const void *v;
 } Arg;
 
-typedef struct {
-	 uint b;
-	 uint mask;
-	 void (*func)(const Arg *);
-	 const Arg arg;
-} MouseKey;
-
 void die(const char *, ...);
 void redraw(void);
 void draw(void);
@@ -127,4 +120,3 @@ extern char *termname;
 extern unsigned int tabspaces;
 extern unsigned int defaultfg;
 extern unsigned int defaultbg;
-extern MouseKey mkeys[];
